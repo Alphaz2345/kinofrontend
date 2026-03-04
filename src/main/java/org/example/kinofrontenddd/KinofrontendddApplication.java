81b8a2ba794f076baf54504278ccfe98b699cb91
zaka23-cyber <petererperker2@gmail.com>
2026-03-04 11:04:57 +0100

    Initial commit

---
+++ src/main/java/org/example/kinofrontenddd/KinofrontendddApplication.java
@@ -0,0 +1,13 @@
+package org.example.kinofrontenddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinofrontendddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinofrontendddApplication.class, args);
+    }
+
+}
